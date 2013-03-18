d5ce033fed6119b4797ef141168d1904b454eef9
srungar2 <srungar2@SWSBEAR.ad.uillinois.edu>
2013-03-18 16:10:39 -0500

    Minor changes for testing.

--- DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
+++ DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
@@ -1,7 +1,24 @@
 package edu.illinois.isws.server;
 
+import static spark.Spark.*;
+import spark.*;
+
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+
+   public static void main(String[] args) {
+      
+      get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }
+      });
+      
+      get(new Route("/test") {
+          @Override
+          public Object handle(Request request, Response response) {
+             return "Testing!";
+          }
+       });
+   }
 }
