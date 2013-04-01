47f51191e764ec024bc12d54058b4e2861dc039e
srungar2 <srungar2@SWSBEAR.ad.uillinois.edu>
2013-04-01 13:09:14 -0500

    Handling file processing and also invoking the executable.

--- DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
+++ DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
@@ -1,6 +1,12 @@
 package edu.illinois.isws.server;
 
 import static spark.Spark.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
 import spark.*;
 import com.google.gson.Gson;
 
@@ -20,18 +26,37 @@ public class HelloWorld {
 			public Object handle(Request request, Response response) {
 				Gson gson = new Gson();
 				InputParameters input = new InputParameters();
-				input = gson.fromJson(request.queryParams("input"), input.getClass());
-///				Runtime runtime = Runtime.getRuntime();
-//				Process process = null;
-//				try {
-//					String executable = "c:\\windows\notpad.exe";
-//					process = runtime.exec(executable);
-//				} catch (Exception e) {
-//					System.out.println("Error: " + e.getMessage());
-//					e.printStackTrace();
-//				}
-				//return request.queryParams(input.txtInOut);
-				return input.txtInOut;
+				Output output = new Output();
+				String inputFileLocation = "";
+				String outputFileLocation = "";
+				String out = null;
+				input = gson.fromJson(request.queryParams("input"),
+						input.getClass());
+
+				try {
+					FileUtils.writeStringToFile(new File(inputFileLocation),
+							input.txtInOut);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				Runtime runtime = Runtime.getRuntime();
+				Process process = null;
+				try {
+					String executable = "";
+					process = runtime.exec(executable);
+				} catch (Exception e) {
+					System.out.println("Error: " + e.getMessage());
+					e.printStackTrace();
+				}
+				try {
+					FileUtils.readFileToString(new File(outputFileLocation),
+							out);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				output.setOutput(out);
+				return gson.toJson(output);
 			}
 		});
 	}
