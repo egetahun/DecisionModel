b8231e71454ff3ab5ac7f0874cc19af2386eac7b
srungar2 <srungar2@SWSBEAR.ad.uillinois.edu>
2013-04-01 12:35:16 -0500

    Deserialzing json objects received from client.

--- DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
+++ DecisonModel/src/edu/illinois/isws/server/HelloWorld.java
@@ -2,23 +2,37 @@ package edu.illinois.isws.server;
 
 import static spark.Spark.*;
 import spark.*;
+import com.google.gson.Gson;
 
 public class HelloWorld {
 
-   public static void main(String[] args) {
-      
-      get(new Route("/hello") {
-         @Override
-         public Object handle(Request request, Response response) {
-            return "Hello World!";
-         }
-      });
-      
-      get(new Route("/test") {
-          @Override
-          public Object handle(Request request, Response response) {
-             return "Testing!";
-          }
-       });
-   }
+	public static void main(String[] args) {
+
+		get(new Route("/hello") {
+			@Override
+			public Object handle(Request request, Response response) {
+				return "Hello World!";
+			}
+		});
+
+		get(new Route("/run") {
+			@Override
+			public Object handle(Request request, Response response) {
+				Gson gson = new Gson();
+				InputParameters input = new InputParameters();
+				input = gson.fromJson(request.queryParams("input"), input.getClass());
+///				Runtime runtime = Runtime.getRuntime();
+//				Process process = null;
+//				try {
+//					String executable = "c:\\windows\notpad.exe";
+//					process = runtime.exec(executable);
+//				} catch (Exception e) {
+//					System.out.println("Error: " + e.getMessage());
+//					e.printStackTrace();
+//				}
+				//return request.queryParams(input.txtInOut);
+				return input.txtInOut;
+			}
+		});
+	}
 }
---
+++ DecisonModel/src/edu/illinois/isws/server/InputParameters.java
@@ -0,0 +1,8 @@
+package edu.illinois.isws.server;
+
+public class InputParameters {
+	public String txtInOut;
+	public InputParameters() {
+		this.txtInOut = "";
+	}
+}
